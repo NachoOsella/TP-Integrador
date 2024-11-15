f08086d5412a176c0e6421a4b197462f1a9e3db7
NachoOsella <nachoosella03@gmail.com>
2024-11-15 00:02:05 -0300

    ultimo commit

--- BackEndApi/Controllers/TicketController.cs
+++ BackEndApi/Controllers/TicketController.cs
@@ -20,54 +20,6 @@ namespace BackEndApi.Controllers
             _repository = repository;
             _context = context;
         }
-
-        // Registrar transacción
-        [HttpPost("RegistrarTransaccion")]
-        public async Task<IActionResult> RegistrarTransaccion([FromBody] FacturaDTO facturaDto)
-        {
-            using var transaction = await _context.Database.BeginTransactionAsync();
-            try
-            {
-                // con los datos de la factura, crear una nueva factura
-                var factura = new Factura
-                {
-                    Monto = facturaDto.Monto,
-                    Fecha = DateTime.Now,
-                    IdFormaDePago = facturaDto.IdFormaDePago,
-                    IdCliente = facturaDto.IdCliente
-                };
-
-                // Agregar la factura al contexto y guardar los cambios
-                _context.Facturas.Add(factura);
-                await _context.SaveChangesAsync();
-
-                // Por cada detalle de factura en la lista de detalles de factura, crear un nuevo detalle de factura
-                foreach (var detalleDto in facturaDto.DetalleFacturas)
-                {
-                    var detalle = new DetalleFactura
-                    {
-                        NroFactura = factura.NroFactura,
-                        NroFuncion = detalleDto.NroFuncion,
-                        CodPromocion = detalleDto.CodPromocion
-                    };
-                    // Agregar el detalle de factura al contexto
-                    _context.DetalleFacturas.Add(detalle);
-                }
-
-                // Guardar los cambios
-                await _context.SaveChangesAsync();
-                // Commit de la transacción
-                await transaction.CommitAsync();
-
-                return Ok("Transacción registrada con éxito");
-            }
-            catch (Exception ex)
-            {
-                await transaction.RollbackAsync();
-                return BadRequest("Error al registrar la transacción: " + ex.Message);
-            }
-        }
-
         // getall formas de pago
         [HttpGet("GetAllFormasDePago")]
         public IActionResult GetAllFormasDePago()
